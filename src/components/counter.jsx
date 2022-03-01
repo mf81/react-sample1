e53c8de29bd87f6e631dd892f9ec071e5d653a48
mf81 <35445517+mf81@users.noreply.github.com>
2022-03-01 17:39:23 +0100

    Make root functionaliy of sample 1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {};
+  state = { counter: 1 };
+
+  handleIncrement = () => {
+    let increment = this.state.counter;
+    increment = increment + 1;
+    this.setState({ counter: increment });
+  };
+
+  handleDecrement = () => {
+    let decrement = this.state.counter;
+    decrement = decrement - 1;
+    this.setState({ counter: decrement });
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="container">
           <h1>React Sample 1</h1>
           <h2>Counter</h2>
+
+          <span className="badge bg-secondary bg-success">
+            {this.state.counter}
+          </span>
+          <button
+            onClick={this.handleIncrement}
+            className="btn btn-secondary btn-sm m-2"
+          >
+            Incrising
+          </button>
+
+          <button
+            onClick={this.handleDecrement}
+            className="btn btn-secondary btn-sm m-2"
+          >
+            Decrising
+          </button>
         </div>
       </React.Fragment>
     );
