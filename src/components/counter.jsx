77a9b38af5831e8e9b25a575cd4c0949f25cefdd
mf81 <35445517+mf81@users.noreply.github.com>
2022-03-01 17:16:49 +0100

    Setup project

---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <h1>React Sample 1</h1>
+          <h2>Counter</h2>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
